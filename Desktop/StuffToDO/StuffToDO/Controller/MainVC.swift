9aef8de6f320cf146b2bf1b91825aa983bab359b
Couto <cs.couto@outlook.com>
2017-10-20 12:51:12 -0200

    [project] - layout/ database

---
+++ Desktop/StuffToDO/StuffToDO/Controller/MainVC.swift
@@ -0,0 +1,13 @@
+//
+//  MainVC.swift
+//  StuffToDO
+//
+//  Created by Couto on 10/20/17.
+//  Copyright © 2017 coutocode. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+}
